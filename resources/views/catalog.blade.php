1fcd1f4816536e22fb50781cb0de3a23c705eb0e
Perceval_blood <Iheardthatwas@gmail.com>
2021-04-14 16:16:42 +0600

    stylish2

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -153,11 +153,11 @@
                                                         <use xlink:href="{{secure_asset('template/images/sprite.svg#layout-grid-16x16')}}"></use>
                                                     </svg>
                                                 </button>
-                                                <button data-layout="grid-3-sidebar" data-with-features="true" title="Grid With Features" type="button" class="layout-switcher__button ">
+                                               {{--  <button data-layout="grid-3-sidebar" data-with-features="true" title="Grid With Features" type="button" class="layout-switcher__button ">
                                                     <svg width="16px" height="16px">
                                                         <use xlink:href="{{secure_asset('template/images/sprite.svg#layout-grid-with-details-16x16')}}"></use>
                                                     </svg>
-                                                </button>
+                                                </button> --}}
                                                 <button data-layout="list" data-with-features="false" title="List" type="button" class="layout-switcher__button ">
                                                     <svg width="16px" height="16px">
                                                         <use xlink:href="{{secure_asset('template/images/sprite.svg#layout-list-16x16')}}"></use>
