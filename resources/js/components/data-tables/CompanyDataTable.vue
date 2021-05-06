bbbabac396d0f5bb945339c2648a3b398c52548a
Nurasyl-Absamat <38110372+Nurasyl-Absamat@users.noreply.github.com>
2021-05-06 21:20:26 +0600

    CompanyDataTable.vue

---
+++ resources/js/components/data-tables/CompanyDataTable.vue
@@ -0,0 +1,38 @@
+<template>
+    <data-table
+        url="https://smartbazar.test/api/companies"
+        :columns="columns">
+
+    </data-table>
+</template>
+
+<script>
+
+
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    },
+    data() {
+        return {
+            perPage: ['10'],
+            columns: [
+                {
+                    label: 'Name',
+                    name: 'name',
+                    searchable: true,
+                    orderable: true,
+                },
+                {
+                    label: 'Phone',
+                    name: 'phone',
+                    searchable: true,
+                    orderable: true,
+
+                },
+
+            ]
+        }
+    },
+}
+</script>
