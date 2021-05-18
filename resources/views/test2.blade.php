6e9fef8119c3f63c807d69a90ed8d11a71bf5f59
AlmazDD <Iheardthatwas@gmail.com>
2021-05-18 19:22:49 +0600

    accordion

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -1,36 +1,39 @@
 @extends('layouts.default')
 
 @section('content')
-<a href="https://smartbazar.kz/">
-    <div class="contained">
-      <span class="category_name">Рыба</span>
-    <div class="cards">
-      <div class="card3">
-        <div class="circular_image">
-          <img src="https://smartbazar-gallery.s3.eu-north-1.amazonaws.com/images/FtT8qpMrwojRVyGYMxTBwzRtlGXPLXUgKv48p3aX.jpg" alt=""/>
+
+    <a href="https://smartbazar.kz/">
+        <div class="contained">
+            <span class="category_name">Рыба</span>
+            <div class="cards">
+                <div class="carding">
+                    <div class="circular_image">
+                        <img src="https://smartbazar-gallery.s3.eu-north-1.amazonaws.com/images/FtT8qpMrwojRVyGYMxTBwzRtlGXPLXUgKv48p3aX.jpg" alt=""/>
+                    </div>
+                    <div class="card__content">
+                        <span> Икринки </span>
+                    </div>
+                </div>
+
+                <div class="carding">
+                    <div class="circular_image">
+                    <img  src="https://arbuz.kz/image/f/233097-krevetki_kingfisher_v_pancire_bez_glazuri_1_kg.jpg?w=260&h=260&_c=1620713344" alt="" >
+                    </div>
+                    <div class="card__content">
+                        <span> Креветки </span>
+                    </div>
+                </div>
+
+                <div class="carding">
+                    <div class="circular_image">
+                        <img  src="https://arbuz.kz/image/f/233097-krevetki_kingfisher_v_pancire_bez_glazuri_1_kg.jpg?w=260&h=260&_c=1620713344" alt="">
+                    </div>
+                    <div class="card__content">
+                        <span> Креветки </span>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="card__content">
-          <span> Икринки </span>
-        </div>
-    </div>
-    
-    <div class="card1">
-        <img class="circular_image" src="https://arbuz.kz/image/f/233097-krevetki_kingfisher_v_pancire_bez_glazuri_1_kg.jpg?w=260&h=260&_c=1620713344" alt="" style="  border-radius: 50%;">
-        <div class="card__content">
-          <span> Креветки </span>
-    </div>
-    
-      </div>
-      <div class="card2">
-        <img class="circular_image" src="https://arbuz.kz/image/f/218978-39-myaso_osminoga_santa_ru.jpg?w=260&h=260&_c=1620713345" alt="">
-        <div class="card__content">
-          <span> Осьминоги </span>
-    
-        </div>
-    
-      </div>
-    </div>
-    </div>
-</a>
+    </a>
 
 @endsection
